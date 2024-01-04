ef87f8bd0832714a73b874a2ad6924a43228ec1e
Humphrey <h.nyahoja@alustudent.com>
2024-01-04 14:48:43 -0800

    jkl

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
-number = random.choice([-98,98])
-if number > 0 :
+number = random.choice([-98, 98])
+if number > 0:
     print(f"{number:d} is positive")
-elif number == 0 :
+elif number == 0:
     print(f"{number:d} is zero")
 else:
     print(f"{number:d} is negative")
